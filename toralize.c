75492eb0c6703025b63a8068f6346cc93f71a8ca
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-19 18:42:28 -0600

    some message

--- toralize.c
+++ toralize.c
@@ -1,7 +1,21 @@
 #include "toralize.h"
 
-int main(){
+int main(int argc, char *argv[]){
+  // To get command line arguments in C, specify first parameter in main 'argc' as int, 
+  // and second parameter as a char array pointer 'argv'
+  char *host;
+  int port;
 
-  printf("Hello World!");
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+    return -1;
+  }
+
+  host = argv[1];
+  port = atoi(argv[2]); // returns 0 if an incorrect type is passed
+  if (port == 0){
+    fprintf(stderr, "The third argument, '%s' needs to be a number\n", argv[2]);
+  }
+
   return 0;
 }
--- toralize.h
+++ toralize.h
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define PROXY "127.0.0.1"
 #define PROXYPORT 9050
+
+typedef unsigned char int8;
+typedef unsigned short int16;
+typedef unsigned int int32;
+
+struct proxyRequest {
+  int8 versionNumber = 4;
+  int8 commandCode;
+
+};
